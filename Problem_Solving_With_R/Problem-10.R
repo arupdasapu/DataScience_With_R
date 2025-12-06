3d6c20c49ce2aa5a9d0e19482c33baa3af47043c
Arup Das Apu <127678412+arupdasapu@users.noreply.github.com>
2025-12-06 22:31:06 +0600

    Add files via upload

---
+++ Problem_Solving_With_R/Problem-10.R
@@ -0,0 +1,5 @@
+# Problem 10: reverse a vector
+v <- c(1,2,3,4,5)
+#rev() returns reversed vector
+rv <- rev(v)
+print(rv)
---
+++ Problem_Solving_With_R/Problem-11.R
@@ -0,0 +1,6 @@
+# Problem 11: largest of three numbers
+a <- 10;  b <- 5; c <- 6 
+
+leargest <- max(a, b, c)
+leargest
+
